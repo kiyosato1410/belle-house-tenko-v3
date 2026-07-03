6be5fb9ada60f1ba1de0481a9439eaa3d353602a
清貴 佐藤 <kiyosato1014@icloud.com>
2026-07-04 03:24:50 +0900

    Firebase設定を反映

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,8 @@
-// Firebaseを使う場合、Firebaseコンソールの設定をここに貼ります。
-// 未設定のままでもローカル保存で動きます。
 window.BELLE_FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyCtj53F9aECPPfP1zQkVg5MPaoKWx7Y_cs",
+  authDomain: "bellehouse-tenko.firebaseapp.com",
+  projectId: "bellehouse-tenko",
+  storageBucket: "bellehouse-tenko.firebasestorage.app",
+  messagingSenderId: "384945751173",
+  appId: "1:384945751173:web:0f6f0b38d63933f85ac17c"
 };
