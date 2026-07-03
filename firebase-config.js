dfb0f6e1c5ec336396ebe7604f2a1276f385767b
清貴 佐藤 <kiyosato1014@icloud.com>
2026-07-04 02:23:45 +0900

    Firebase設定追加

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+window.BELLE_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCtj53F9aECPPfP1zQkVg5MPaoKWx7Y_cs",
+  authDomain: "bellehouse-tenko.firebaseapp.com",
+  projectId: "bellehouse-tenko",
+  storageBucket: "bellehouse-tenko.firebasestorage.app",
+  messagingSenderId: "384945751173",
+  appId: "1:384945751173:web:0f6f0b38d63933f85ac17c"
+};
